8553a9af2dabdcdb98337a39b0b824c4c74196ab
Rahul Mahawar <68722733+Ardhweb@users.noreply.github.com>
2023-01-28 16:06:06 +0530

    Update wsgi.py

--- badaresellar/wsgi.py
+++ badaresellar/wsgi.py
@@ -8,9 +8,24 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
+
+
+# assuming your Django settings file is at '/home/myusername/mysite/mysite/settings.py'
+path = '/home/badaresellar/badaresellar'
+if path not in sys.path:
+    sys.path.insert(0, path)
+    
+os.environ['DJANGO_SETTINGS_MODULE'] = 'badaresellar.settings'
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'badaresellar.settings')
 
 application = get_wsgi_application()
+
+
+
+
+
+
+
