a8b845c99e93c0558a19ea3f5dd049de1ae071bc
Rahul Mahawar <68722733+Ardhweb@users.noreply.github.com>
2023-01-29 11:49:18 +0530

    Update settings.py

--- badaresellar/settings.py
+++ badaresellar/settings.py
@@ -184,12 +184,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com' 
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'badareseller@gmail.com'  
-EMAIL_HOST_PASSWORD = 'zxyxiizuiusius'
+EMAIL_HOST_USER = 'email@gmail.com'  
+EMAIL_HOST_PASSWORD = 'App Pawssord Code @ step Verification One'
 
 DEFAULT_FROM_EMAIL = '<noreply@badaresellar.com>'
 
-RECIPIENT_ADDRESS = 'badareseller@gmail.com'
+RECIPIENT_ADDRESS = 'email@gmail.com'
 
 
 
